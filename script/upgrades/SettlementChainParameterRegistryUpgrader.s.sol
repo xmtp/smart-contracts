a480cc4569341028a046b8de4086d4976d2f303d
Martin Kysel <code@martinkysel.com>
2025-10-28 11:42:05 -0700

    upgrades

--- script/upgrades/SettlementChainParameterRegistryUpgrader.s.sol
+++ script/upgrades/SettlementChainParameterRegistryUpgrader.s.sol
@@ -7,19 +7,10 @@ import { GenericEIP1967Migrator } from "../../../src/any-chain/GenericEIP1967Mig
 import { IERC1967 } from "../../../src/abstract/interfaces/IERC1967.sol";
 
 import { IParameterRegistry } from "../../../src/abstract/interfaces/IParameterRegistry.sol";
-import { SettlementChainGateway } from "../../../src/settlement-chain/SettlementChainGateway.sol";
-import { SettlementChainGatewayDeployer } from "../../../script/deployers/SettlementChainGatewayDeployer.sol";
 import { Utils } from "../../script/utils/Utils.sol";
+import {SettlementChainParameterRegistryDeployer} from "../deployers/SettlementChainParameterRegistryDeployer.sol";
 
-interface ISettlementChainGateway {
-    function parameterRegistry() external view returns (address);
-    function appChainGateway() external view returns (address);
-    function feeToken() external view returns (address);
-    function migrate() external;
-    function migratorParameterKey() external pure returns (string memory);
-}
-
-contract SettlementChainGatewayUpgrader is Script {
+contract SettlementChainParameterRegistryUpgrader is Script {
     error PrivateKeyNotSet();
     error EnvironmentNotSet();
 
@@ -48,33 +39,24 @@ contract SettlementChainGatewayUpgrader is Script {
         console.log("Admin: %s", _admin);
     }
 
-    function UpgradeSettlementChainGateway() external {
+    function UpgradeSettlementParameterRegistry() external {
         address factory = deployment.factory;
         console.log("factory" , factory);
         address paramRegistry = deployment.parameterRegistryProxy;
         console.log("paramRegistry" , paramRegistry);
-        address settlementChainGateway = deployment.gatewayProxy;
-        console.log("settlementChainGateway" , settlementChainGateway);
-        address feeToken = deployment.feeTokenProxy;
-        console.log("feeToken" , feeToken);
-        address proxy = deployment.gatewayProxy;
-        console.log("proxy" , proxy);
 
         vm.startBroadcast(_privateKey);
-        (address newImpl, ) = SettlementChainGatewayDeployer.deployImplementation(
-            factory,
-            paramRegistry,
-            settlementChainGateway,
-            feeToken
+        (address newImpl, ) = SettlementChainParameterRegistryDeployer.deployImplementation(
+            factory
         );
         console.log("newImpl", newImpl);
 
         GenericEIP1967Migrator migrator = new GenericEIP1967Migrator(newImpl);
         console.log("migrator", address(migrator));
-        string memory key = ISettlementChainGateway(proxy).migratorParameterKey();
+        string memory key = IParameterRegistry(paramRegistry).migratorParameterKey();
         IParameterRegistry(paramRegistry).set(key, bytes32(uint256(uint160(address(migrator)))));
 
-        SettlementChainGateway(settlementChainGateway).migrate();
+        IParameterRegistry(paramRegistry).migrate();
 
         vm.stopBroadcast();
     }
