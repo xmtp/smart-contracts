6be92472e8c930977001892f37a05eccd8e68083
Kevin Small <KevinSmall@users.noreply.github.com>
2025-11-20 11:46:02 +0000

    refactor to have base upgrader, add contract state fields to payer registry upgrader

--- script/upgrades/NodeRegistryUpgrader.s.sol
+++ script/upgrades/NodeRegistryUpgrader.s.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import { Script, console } from "../../lib/forge-std/src/Script.sol";
-import { VmSafe } from "../../lib/forge-std/src/Vm.sol";
-import { GenericEIP1967Migrator } from "../../src/any-chain/GenericEIP1967Migrator.sol";
+import { console } from "../../lib/forge-std/src/Script.sol";
 import { NodeRegistry } from "../../src/settlement-chain/NodeRegistry.sol";
-import { IParameterRegistry } from "../../src/abstract/interfaces/IParameterRegistry.sol";
-import { Utils } from "../utils/Utils.sol";
+import { BaseUpgrader } from "./BaseUpgrader.s.sol";
 import { NodeRegistryDeployer } from "../deployers/NodeRegistryDeployer.sol";
 
 /**
@@ -23,11 +20,7 @@ import { NodeRegistryDeployer } from "../deployers/NodeRegistryDeployer.sol";
  *   ENVIRONMENT=testnet-dev forge script script/upgrades/NodeRegistryUpgrader.s.sol:NodeRegistryUpgrader --rpc-url base_sepolia --slow --sig "UpgradeNodeRegistry()" --broadcast
  *
  */
-contract NodeRegistryUpgrader is Script {
-    error PrivateKeyNotSet();
-    error EnvironmentNotSet();
-    error StateMismatch();
-
+contract NodeRegistryUpgrader is BaseUpgrader {
     struct ContractState {
         address parameterRegistry;
         uint8 canonicalNodesCount;
@@ -36,75 +29,71 @@ contract NodeRegistryUpgrader is Script {
         string version;
     }
 
-    string internal _environment;
-    uint256 internal _privateKey;
-    address internal _admin;
-    Utils.DeploymentData internal _deployment;
-    ContractState internal _contractStateBefore;
-
-    function setUp() external {
-        // Environment
-        _environment = vm.envString("ENVIRONMENT");
-        if (bytes(_environment).length == 0) revert EnvironmentNotSet();
-        console.log("Environment: %s", _environment);
-
-        // Admin private key
-        _deployment = Utils.parseDeploymentData(string.concat("config/", _environment, ".json"));
-        _privateKey = uint256(vm.envBytes32("ADMIN_PRIVATE_KEY"));
-        if (_privateKey == 0) revert PrivateKeyNotSet();
-        _admin = vm.addr(_privateKey);
-        console.log("Admin: %s", _admin);
-
-        // Contract state before upgrade
-        address proxy = _deployment.nodeRegistryProxy;
-        _contractStateBefore = getContractState(proxy);
+    function UpgradeNodeRegistry() external {
+        _upgrade();
     }
 
-    function UpgradeNodeRegistry() external {
-        address factory = _deployment.factory;
-        console.log("factory %s", factory);
-        address paramRegistry = _deployment.parameterRegistryProxy;
-        console.log("paramRegistry %s", paramRegistry);
-        address proxy = _deployment.nodeRegistryProxy;
-        console.log("proxy %s", proxy);
+    function _getProxy() internal view override returns (address proxy_) {
+        return _deployment.nodeRegistryProxy;
+    }
 
-        vm.startBroadcast(_privateKey);
+    function _deployOrGetImplementation() internal override returns (address implementation_) {
+        address factory = _deployment.factory;
+        address paramRegistry = _deployment.parameterRegistryProxy;
 
         // Compute implementation address
         address computedImpl = NodeRegistryDeployer.getImplementation(factory, paramRegistry);
-        address newImpl;
 
         // Skip deployment if implementation already exists
         if (computedImpl.code.length > 0) {
             console.log("Implementation already exists at computed address, skipping deployment");
-            newImpl = computedImpl;
-        } else {
-            // Deploy new implementation, can be called by ANY address
-            (newImpl, ) = NodeRegistryDeployer.deployImplementation(factory, paramRegistry);
+            return computedImpl;
         }
-        console.log("newImpl %s", newImpl);
 
-        // Deploy generic migrator, can be called by ANY address
-        GenericEIP1967Migrator migrator = new GenericEIP1967Migrator(newImpl);
-        console.log("migrator (also param reg migrator value) %s", address(migrator));
-
-        // Parameter registry stores the migrator address, must be called by a param registry admin address
-        string memory key = NodeRegistry(proxy).migratorParameterKey();
-        IParameterRegistry(paramRegistry).set(key, bytes32(uint256(uint160(address(migrator)))));
-        console.log("param reg migrator key %s", key);
-
-        // Perform migration, can be called by any address
-        NodeRegistry(proxy).migrate();
-        vm.stopBroadcast();
-
-        // Compare state before and after upgrade
-        ContractState memory contractStateAfter = getContractState(proxy);
-        _logContractState("State before upgrade:", _contractStateBefore);
-        _logContractState("State after upgrade:", contractStateAfter);
-        if (!isContractStateEqual(_contractStateBefore, contractStateAfter)) revert StateMismatch();
+        // Deploy new implementation
+        (implementation_, ) = NodeRegistryDeployer.deployImplementation(factory, paramRegistry);
     }
 
-    function getContractState(address proxy_) public view returns (ContractState memory state_) {
+    function _getMigratorParameterKey(address proxy_) internal pure override returns (string memory key_) {
+        return NodeRegistry(proxy_).migratorParameterKey();
+    }
+
+    function _getContractState(address proxy_) internal view override returns (bytes memory state_) {
+        ContractState memory state = _getNodeRegistryState(proxy_);
+        return abi.encode(state);
+    }
+
+    function _isContractStateEqual(
+        bytes memory stateBefore_,
+        bytes memory stateAfter_
+    ) internal pure override returns (bool isEqual_) {
+        ContractState memory before = abi.decode(stateBefore_, (ContractState));
+        ContractState memory afterState = abi.decode(stateAfter_, (ContractState));
+
+        isEqual_ =
+            before.parameterRegistry == afterState.parameterRegistry &&
+            before.canonicalNodesCount == afterState.canonicalNodesCount &&
+            before.nodeCount == afterState.nodeCount;
+
+        // Only check contractName if it existed in the before state (non-empty)
+        // This handles upgrades from old versions without contractName to new versions with it
+        if (bytes(before.contractName).length > 0) {
+            isEqual_ = isEqual_ && keccak256(bytes(before.contractName)) == keccak256(bytes(afterState.contractName));
+        }
+        // Note: version is intentionally not checked, it can change
+    }
+
+    function _logContractState(string memory title_, bytes memory state_) internal view override {
+        ContractState memory state = abi.decode(state_, (ContractState));
+        console.log("%s", title_);
+        console.log("  Parameter registry: %s", state.parameterRegistry);
+        console.log("  Canonical nodes count: %s", uint256(state.canonicalNodesCount));
+        console.log("  Node count: %s", uint256(state.nodeCount));
+        console.log("  Name: %s", state.contractName);
+        console.log("  Version: %s", state.version);
+    }
+
+    function _getNodeRegistryState(address proxy_) internal view returns (ContractState memory state_) {
         NodeRegistry nodeRegistry = NodeRegistry(proxy_);
         state_.parameterRegistry = nodeRegistry.parameterRegistry();
         state_.canonicalNodesCount = nodeRegistry.canonicalNodesCount();
@@ -124,29 +113,25 @@ contract NodeRegistryUpgrader is Script {
         }
     }
 
+    // Public functions for testing
+    function getContractState(address proxy_) public view returns (ContractState memory state_) {
+        return _getNodeRegistryState(proxy_);
+    }
+
     function isContractStateEqual(
         ContractState memory before_,
-        ContractState memory after_
+        ContractState memory afterState_
     ) public pure returns (bool isEqual_) {
         isEqual_ =
-            before_.parameterRegistry == after_.parameterRegistry &&
-            before_.canonicalNodesCount == after_.canonicalNodesCount &&
-            before_.nodeCount == after_.nodeCount;
+            before_.parameterRegistry == afterState_.parameterRegistry &&
+            before_.canonicalNodesCount == afterState_.canonicalNodesCount &&
+            before_.nodeCount == afterState_.nodeCount;
 
         // Only check contractName if it existed in the before state (non-empty)
         // This handles upgrades from old versions without contractName to new versions with it
         if (bytes(before_.contractName).length > 0) {
-            isEqual_ = isEqual_ && keccak256(bytes(before_.contractName)) == keccak256(bytes(after_.contractName));
+            isEqual_ = isEqual_ && keccak256(bytes(before_.contractName)) == keccak256(bytes(afterState_.contractName));
         }
         // Note: version is intentionally not checked, it can change
-    }
-
-    function _logContractState(string memory title_, ContractState memory state_) internal view {
-        console.log("%s", title_);
-        console.log("  Parameter registry: %s", state_.parameterRegistry);
-        console.log("  Canonical nodes count: %u", uint256(state_.canonicalNodesCount));
-        console.log("  Node count: %u", uint256(state_.nodeCount));
-        console.log("  Name: %s", state_.contractName);
-        console.log("  Version: %s", state_.version);
     }
 }
--- script/upgrades/PayerRegistryUpgrader.s.sol
+++ script/upgrades/PayerRegistryUpgrader.s.sol
@@ -1,66 +1,154 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import { Script, console } from "../../lib/forge-std/src/Script.sol";
-
-import { GenericEIP1967Migrator } from "../../src/any-chain/GenericEIP1967Migrator.sol";
-import { IERC1967 } from "../../src/abstract/interfaces/IERC1967.sol";
+import { console } from "../../lib/forge-std/src/Script.sol";
 import { PayerRegistry } from "../../src/settlement-chain/PayerRegistry.sol";
-
-import { IParameterRegistry } from "../../src/abstract/interfaces/IParameterRegistry.sol";
-import { Utils } from "../utils/Utils.sol";
+import { BaseUpgrader } from "./BaseUpgrader.s.sol";
 import { PayerRegistryDeployer } from "../deployers/PayerRegistryDeployer.sol";
 
-contract PayerRegistryUpgrader is Script {
-    error PrivateKeyNotSet();
-    error EnvironmentNotSet();
-
-    string internal _environment;
-
-    uint256 internal _privateKey;
-    address internal _admin;
-
-    Utils.DeploymentData internal deployment;
-
-    function setUp() external {
-        _environment = vm.envString("ENVIRONMENT");
-
-        if (bytes(_environment).length == 0) revert EnvironmentNotSet();
-
-        console.log("Environment: %s", _environment);
-
-        deployment = Utils.parseDeploymentData(string.concat("config/", _environment, ".json"));
-
-        _privateKey = uint256(vm.envBytes32("ADMIN_PRIVATE_KEY"));
-
-        if (_privateKey == 0) revert PrivateKeyNotSet();
-
-        _admin = vm.addr(_privateKey);
-
-        console.log("Admin: %s", _admin);
+contract PayerRegistryUpgrader is BaseUpgrader {
+    struct ContractState {
+        address parameterRegistry;
+        address feeToken;
+        address settler;
+        address feeDistributor;
+        bool paused;
+        int104 totalDeposits;
+        uint96 totalDebt;
+        uint96 minimumDeposit;
+        uint32 withdrawLockPeriod;
+        string contractName;
+        string version;
     }
 
     function UpgradePayerRegistry() external {
-        address factory = deployment.factory;
-        console.log("factory", factory);
-        address paramRegistry = deployment.parameterRegistryProxy;
-        console.log("paramRegistry", paramRegistry);
-        address feeToken = deployment.feeTokenProxy;
-        console.log("feeToken", feeToken);
-        address proxy = deployment.payerRegistryProxy;
-        console.log("proxy", proxy);
+        _upgrade();
+    }
 
-        vm.startBroadcast(_privateKey);
-        (address newImpl, ) = PayerRegistryDeployer.deployImplementation(factory, paramRegistry, feeToken);
-        console.log("newImpl", newImpl);
+    function _getProxy() internal view override returns (address proxy_) {
+        return _deployment.payerRegistryProxy;
+    }
 
-        GenericEIP1967Migrator migrator = new GenericEIP1967Migrator(newImpl);
-        console.log("migrator", address(migrator));
-        string memory key = PayerRegistry(proxy).migratorParameterKey();
-        IParameterRegistry(paramRegistry).set(key, bytes32(uint256(uint160(address(migrator)))));
+    function _deployOrGetImplementation() internal override returns (address implementation_) {
+        address factory = _deployment.factory;
+        address paramRegistry = _deployment.parameterRegistryProxy;
+        address feeToken = _deployment.feeTokenProxy;
 
-        PayerRegistry(proxy).migrate();
+        // Compute implementation address
+        address computedImpl = PayerRegistryDeployer.getImplementation(factory, paramRegistry, feeToken);
 
-        vm.stopBroadcast();
+        // Skip deployment if implementation already exists
+        if (computedImpl.code.length > 0) {
+            console.log("Implementation already exists at computed address, skipping deployment");
+            return computedImpl;
+        }
+
+        // Deploy new implementation
+        (implementation_, ) = PayerRegistryDeployer.deployImplementation(factory, paramRegistry, feeToken);
+    }
+
+    function _getMigratorParameterKey(address proxy_) internal view override returns (string memory key_) {
+        return PayerRegistry(proxy_).migratorParameterKey();
+    }
+
+    function _getContractState(address proxy_) internal view override returns (bytes memory state_) {
+        ContractState memory state = _getPayerRegistryState(proxy_);
+        return abi.encode(state);
+    }
+
+    function _isContractStateEqual(
+        bytes memory stateBefore_,
+        bytes memory stateAfter_
+    ) internal pure override returns (bool isEqual_) {
+        ContractState memory before = abi.decode(stateBefore_, (ContractState));
+        ContractState memory afterState = abi.decode(stateAfter_, (ContractState));
+
+        isEqual_ =
+            before.parameterRegistry == afterState.parameterRegistry &&
+            before.feeToken == afterState.feeToken &&
+            before.settler == afterState.settler &&
+            before.feeDistributor == afterState.feeDistributor &&
+            before.paused == afterState.paused &&
+            before.totalDeposits == afterState.totalDeposits &&
+            before.totalDebt == afterState.totalDebt &&
+            before.minimumDeposit == afterState.minimumDeposit &&
+            before.withdrawLockPeriod == afterState.withdrawLockPeriod;
+
+        // Only check contractName if it existed in the before state (non-empty)
+        // This handles upgrades from old versions without contractName to new versions with it
+        if (bytes(before.contractName).length > 0) {
+            isEqual_ = isEqual_ && keccak256(bytes(before.contractName)) == keccak256(bytes(afterState.contractName));
+        }
+        // Note: version is intentionally not checked, it can change
+    }
+
+    function _logContractState(string memory title_, bytes memory state_) internal view override {
+        ContractState memory state = abi.decode(state_, (ContractState));
+        console.log("%s", title_);
+        console.log("  Parameter registry: %s", state.parameterRegistry);
+        console.log("  Fee token: %s", state.feeToken);
+        console.log("  Settler: %s", state.settler);
+        console.log("  Fee distributor: %s", state.feeDistributor);
+        console.log("  Paused: %s", state.paused);
+        console.log("  Total deposits: %s", uint256(uint104(state.totalDeposits)));
+        console.log("  Total debt: %s", uint256(state.totalDebt));
+        console.log("  Minimum deposit: %s", uint256(state.minimumDeposit));
+        console.log("  Withdraw lock period: %s", uint256(state.withdrawLockPeriod));
+        console.log("  Name: %s", state.contractName);
+        console.log("  Version: %s", state.version);
+    }
+
+    function _getPayerRegistryState(address proxy_) internal view returns (ContractState memory state_) {
+        PayerRegistry payerRegistry = PayerRegistry(proxy_);
+        state_.parameterRegistry = payerRegistry.parameterRegistry();
+        state_.feeToken = payerRegistry.feeToken();
+        state_.settler = payerRegistry.settler();
+        state_.feeDistributor = payerRegistry.feeDistributor();
+        state_.paused = payerRegistry.paused();
+        state_.totalDeposits = payerRegistry.totalDeposits();
+        state_.totalDebt = payerRegistry.totalDebt();
+        state_.minimumDeposit = payerRegistry.minimumDeposit();
+        state_.withdrawLockPeriod = payerRegistry.withdrawLockPeriod();
+
+        // Try to get contractName and version, which may not exist in older implementations
+        try payerRegistry.contractName() returns (string memory contractName_) {
+            state_.contractName = contractName_;
+        } catch {
+            state_.contractName = "";
+        }
+
+        try payerRegistry.version() returns (string memory version_) {
+            state_.version = version_;
+        } catch {
+            state_.version = "";
+        }
+    }
+
+    // Public functions for testing
+    function getContractState(address proxy_) public view returns (ContractState memory state_) {
+        return _getPayerRegistryState(proxy_);
+    }
+
+    function isContractStateEqual(
+        ContractState memory before_,
+        ContractState memory afterState_
+    ) public pure returns (bool isEqual_) {
+        isEqual_ =
+            before_.parameterRegistry == afterState_.parameterRegistry &&
+            before_.feeToken == afterState_.feeToken &&
+            before_.settler == afterState_.settler &&
+            before_.feeDistributor == afterState_.feeDistributor &&
+            before_.paused == afterState_.paused &&
+            before_.totalDeposits == afterState_.totalDeposits &&
+            before_.totalDebt == afterState_.totalDebt &&
+            before_.minimumDeposit == afterState_.minimumDeposit &&
+            before_.withdrawLockPeriod == afterState_.withdrawLockPeriod;
+
+        // Only check contractName if it existed in the before state (non-empty)
+        // This handles upgrades from old versions without contractName to new versions with it
+        if (bytes(before_.contractName).length > 0) {
+            isEqual_ = isEqual_ && keccak256(bytes(before_.contractName)) == keccak256(bytes(afterState_.contractName));
+        }
+        // Note: version is intentionally not checked, it can change
     }
 }
