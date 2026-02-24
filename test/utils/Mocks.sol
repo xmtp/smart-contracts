95ec89a4807cb015dd35ca6bb6ebe941b5ee7f83
Kevin Small <KevinSmall@users.noreply.github.com>
2026-02-24 16:57:02 +0000

    refactor test

--- test/utils/Mocks.sol
+++ test/utils/Mocks.sol
@@ -5,9 +5,12 @@ import {
     ERC20PermitUpgradeable
 } from "../../lib/oz-upgradeable/contracts/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 
+import { EnumerableSet } from "../../lib/oz/contracts/utils/structs/EnumerableSet.sol";
+
 import { RegistryParameters } from "../../src/libraries/RegistryParameters.sol";
 
 import { IERC1967 } from "../../src/abstract/interfaces/IERC1967.sol";
+import { INodeRegistry } from "../../src/settlement-chain/interfaces/INodeRegistry.sol";
 
 import { Migratable } from "../../src/abstract/Migratable.sol";
 
@@ -57,5 +60,69 @@ contract MockUnderlyingFeeToken is Migratable, ERC20PermitUpgradeable {
 
     function migratorParameterKey() public pure returns (string memory key_) {
         return "xmtp.mockUnderlyingFeeToken.migrator";
+    }
+}
+
+contract MockNodeRegistryProxy {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    // keccak256(abi.encode(uint256(keccak256("xmtp.storage.NodeRegistry")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 internal constant _NODE_REGISTRY_STORAGE_LOCATION =
+        0xd48713bc7b5e2644bcb4e26ace7d67dc9027725a9a1ee11596536cc6096a2000;
+
+    struct NodeRegistryStorage {
+        address admin;
+        uint8 maxCanonicalNodes;
+        uint8 canonicalNodesCount;
+        uint32 nodeCount;
+        mapping(uint32 nodeId => INodeRegistry.Node node) nodes;
+        string baseURI;
+        EnumerableSet.UintSet canonicalNodes;
+    }
+
+    function _getStorage() internal pure returns (NodeRegistryStorage storage $) {
+        assembly {
+            $.slot := _NODE_REGISTRY_STORAGE_LOCATION
+        }
+    }
+
+    function setImplementation(address implementation_) external {
+        assembly {
+            sstore(0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc, implementation_)
+        }
+    }
+
+    function callMigrator(address migrator_) external {
+        (bool success_, bytes memory data_) = migrator_.delegatecall("");
+
+        if (success_) return;
+
+        assembly {
+            revert(add(data_, 0x20), mload(data_))
+        }
+    }
+
+    function setNodeCount(uint32 count_) external {
+        _getStorage().nodeCount = count_;
+    }
+
+    function setNodeCanonical(uint32 nodeId_) external {
+        _getStorage().nodes[nodeId_].isCanonical = true;
+    }
+
+    function setNodeNonCanonical(uint32 nodeId_) external {
+        _getStorage().nodes[nodeId_].isCanonical = false;
+    }
+
+    function addToCanonicalSet(uint32 nodeId_) external {
+        _getStorage().canonicalNodes.add(nodeId_);
+    }
+
+    function getCanonicalNodesCount() external view returns (uint256 count_) {
+        return _getStorage().canonicalNodes.length();
+    }
+
+    function isInCanonicalSet(uint32 nodeId_) external view returns (bool isIn_) {
+        return _getStorage().canonicalNodes.contains(nodeId_);
     }
 }
